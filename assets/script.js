db5cd1682c1fbc24e973bdd62e037603b6b3221a
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-08-05 20:30:40 -0500

    I have sucessfully created all user input parameters and have created the arrays for each parameter

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,52 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 
 function generatePassword(){
+  // Obtain User parameters
+  let characterLength = prompt("Enter the desired character length of the password: ");
+
+    if (!characterLength){
+      return "Good Luck creating your own.";
+    }
+    console.log("desired character length of password is: "+characterLength);
+
+  let specialCrts = window.confirm("Include special characters in password?");
+    console.log("specialCrts value is:" + specialCrts);
+
+  let numericCrts = window.confirm("Include special characters in password?");
+    console.log("numericCrts value is:" + numericCrts);
+
+  let lowerCrts = window.confirm("Include special characters in password?");
+    console.log("lowerCrts value is:" + lowerCrts);
+
+  let upperCrts = window.confirm("Include special characters in password?");
+    console.log("upperCrts value is:" + upperCrts);
 
 
+  //Utilize the user input to generate the array we will chose random values from
+  let specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  let numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  let lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+  let genArray = [];
 
-
-
-
-
-
+  if (specialCrts === true) {
+    let genArray = [...genArray, ...specialArray];
+  };
+  console.log(genArray);
 
   
 return "test";
-
 }
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
