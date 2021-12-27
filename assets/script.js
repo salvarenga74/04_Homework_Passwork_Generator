d399368d6b63e8d99e0a5314cc9d3072d543aa49
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-12-26 19:31:16 -0600

    CHanging the prompt of the first alert

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,123 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-
-function generatePassword(){
+function generatePassword() {
   // Obtain User parameters
-  let passwordLength = prompt("Enter the desired password length of the password: ");
+  let passwordLength = prompt(
+    "What is the desired character length (Longer than 7 characters): "
+  );
 
-    if (passwordLength<8){
-      window.alert("Minimum of 8 characters is required.")
-      return [];
-    }
-    if (passwordLength>128){
-      window.alert("No more than 128 characters is allowed.")
-      return [];
-    }
+  if (passwordLength < 8) {
+    window.alert("Minimum of 8 characters is required.");
+    return [];
+  }
+  if (passwordLength > 128) {
+    window.alert("No more than 128 characters is allowed.");
+    return [];
+  }
 
-    console.log("desired password length of password is: "+passwordLength);
+  console.log("desired password length of password is: " + passwordLength);
 
   let specialCrts = window.confirm("Include SPECIAL CHARACTERS in password?");
-    console.log("specialCrts value is:" + specialCrts);
+  console.log("specialCrts value is:" + specialCrts);
 
   let numericCrts = window.confirm("Include NUMERIC CHARACTERS in password?");
-    console.log("numericCrts value is:" + numericCrts);
+  console.log("numericCrts value is:" + numericCrts);
 
   let lowerCrts = window.confirm("Include LOWER CASE LETTERS in password?");
-    console.log("lowerCrts value is:" + lowerCrts);
+  console.log("lowerCrts value is:" + lowerCrts);
 
   let upperCrts = window.confirm("Include UPPER CASE LETTERS in password?");
-    console.log("upperCrts value is:" + upperCrts);
-
+  console.log("upperCrts value is:" + upperCrts);
 
   //Utilize the user input to generate the array we will chose random values from
   let specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
   let numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  let lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let lowerArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let upperArray = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
   let genArray = [];
 
   if (specialCrts === true) {
-     genArray = [...genArray, ...specialArray];
+    genArray = [...genArray, ...specialArray];
   }
   if (numericCrts) {
     genArray = [...genArray, ...numericArray];
   }
- if (lowerCrts) {
-  genArray = [...genArray, ...lowerArray];
+  if (lowerCrts) {
+    genArray = [...genArray, ...lowerArray];
   }
   if (upperCrts) {
-  genArray = [...genArray, ...upperArray];
+    genArray = [...genArray, ...upperArray];
   }
-    console.log("The gen array so far"+genArray);
-
+  console.log("The gen array so far" + genArray);
 
   // Creating a for loop that generate the desired amount of characters into the final array based on user input.
   var finalArray = [];
-  for (let i=0; i<passwordLength; i++){
+  for (let i = 0; i < passwordLength; i++) {
     // Time to randomly choose elements from the genArray to create our custom password.
-    let randomIndexVal = Math.floor(Math.random()*genArray.length);
-        console.log("random index number is "+ randomIndexVal);
+    let randomIndexVal = Math.floor(Math.random() * genArray.length);
+    console.log("random index number is " + randomIndexVal);
 
     var chosenEl = genArray[randomIndexVal];
-      console.log("The randomly chosen element is " + chosenEl);
+    console.log("The randomly chosen element is " + chosenEl);
     finalArray.push(chosenEl);
   }
-    console.log("The final array is "+ finalArray);
+  console.log("The final array is " + finalArray);
 
   // Converting array to a string
   return finalArray.join(" ");
